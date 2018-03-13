c9abc9c9fe6c2d04e88bdc468a330ae4b18b5278
wangxuefeng@yuntutv.net <wangxuefeng@yuntutv.net>
2018-03-13 14:15:27 +0800

    first commit

---
+++ MyCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCode
+//
+//  Created by wangxuefeng on 2018/3/12.
+//  Copyright © 2018年 wangxuefeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCode
+//
+//  Created by wangxuefeng on 2018/3/12.
+//  Copyright © 2018年 wangxuefeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
